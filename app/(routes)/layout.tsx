3bd0f867211ed8dbcf708dad4900d05c9e9bb2cf
BurhanShaikh-9 <shaikhshady6@gmail.com>
2023-10-28 03:52:34 -0700

    update

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/header/header'
+import { Footer } from '@/components/footer/footer'
 import '../_styles/globals.css'
 import type { Metadata } from 'next'
 
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={`${roboto.className} bodyClass`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
